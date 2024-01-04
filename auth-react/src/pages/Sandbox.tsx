d8d7221e760cbabc4ba7ada1ab9a05b28f5218a5
karosuwindam <karosuwindam@gmail.com>
2024-01-05 08:45:28 +0900

    update

--- auth-react/src/pages/Sandbox.tsx
+++ auth-react/src/pages/Sandbox.tsx
@@ -4,6 +4,22 @@ import { Link } from "react-router-dom";
 import { useState, useContext, useEffect } from "react";
 import { GetUser } from "../modules/UserEdit";
 
+interface UserRerister {
+  name: string;
+  password: string;
+  authority: number;
+}
+
+export const UserAdd: React.FC = () => {
+  const [user, setUser] = useState <UserRerister | null>(null);
+  return (
+    <div>
+      <div>user add</div>
+      <div>add buttoon</div>
+    </div>
+  )
+}
+
 export const Sandbox = () => {
   const json = GetUser(1);
   console.log(json);
@@ -11,6 +27,7 @@ export const Sandbox = () => {
   return (
     <div>
       <h1>Sandbox</h1>
+      <UserAdd />
       <h2>view test module</h2>
       <Link to="/" className="App-link">
         back to index
